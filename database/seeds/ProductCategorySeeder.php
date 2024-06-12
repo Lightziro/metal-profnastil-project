6f60041575340069476a67720c0a3ad18f04c5a6
Lightziro <butorin@ittest-team.ru>
2024-06-12 12:42:28 +0300

    init product category seeder

---
+++ database/seeds/ProductCategorySeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ProductCategory;
+use Illuminate\Database\Seeder;
+
+class ProductCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [
+            [
+                'name' => 'Профнастил',
+                'slug' => 'prof_list'
+            ],
+            [
+                'name' => 'Металочерепица',
+                'slug' => 'metal_list'
+            ],
+            [
+                'name' => 'Гладкий лист',
+                'slug' => 'smooth_list'
+            ],
+            [
+                'name' => 'Саморезы',
+                'slug' => 'screw'
+            ]
+        ];
+        foreach ($categories as $category) {
+            ProductCategory::query()->firstOrCreate($category);
+        }
+    }
+}
