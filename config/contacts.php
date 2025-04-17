37a74a3feec905b1bbb1c31aef70968f1a40fdda
Lightziro <butorin@ittest-team.ru>
2025-04-17 11:22:52 +0300

    fix email list config

--- config/contacts.php
+++ config/contacts.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'send_to_email' => ['proflist43@yandex.ru', 'ithub-company@yandex.ru'],
+    'send_to_email' => ['proflist43@yandex.ru', 'kzmp.kirov@yandex.ru', 'a.svet43@yandex.ru'],
     'send_question_users' => ['proflist43@yandex.ru'],
 
 ];
